a2522b0a0666ac3d7a481f95be1436ad9b0ccdea
Dan Rapp <drapp@proofpoint.com>
2018-07-27 16:02:00 -0600

    remove cov spec on pytest (trying to get this working on travis)

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pytest --cov=dockerutils --cov-report=term-missing --cov-fail-under=75 --durations=10 tests
+pytest --cov --cov-report=term-missing --cov-fail-under=75 --durations=10 tests
